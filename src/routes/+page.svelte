8e66801d7361b06e6747edb7ca5e533ee6214e60
Dheemanth A.N. <dheemanth.simha01@gmail.com>
2023-12-09 18:55:24 +0530

    Make changes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,16 +2,16 @@
   import { onMount } from "svelte";
   import { ethers } from "ethers";
   import ABI from "../Whistleblower.json";
-  import Icon from 'svelte-icons-pack/Icon.svelte';
+  import Icon from "svelte-icons-pack/Icon.svelte";
   import BiUpvote from "svelte-icons-pack/bi/BiUpvote";
   import BiDownvote from "svelte-icons-pack/bi/BiDownvote";
   let window2, provider;
-  let CONTRACT_ADDRESS = "0xd9145cce52d386f254917e481eb44e9943f39138";
+  let CONTRACT_ADDRESS = "0xE27E7fB383F12e2e7D97bF5e518c40bE837c468d";
   onMount(async () => {
     window2 = window;
     console.log(window2.ethereum);
 
-    //   let ANTI_ADDRESS = 0xd9145cce52d386f254917e481eb44e9943f39138;
+    //   let ANTI_ADDRESS = 0xE27E7fB383F12e2e7D97bF5e518c40bE837c468d;
     //   let ANTI_ABI = ABI;
     //   const provider = new ethers.providers.Web3Provider(window2.ethereum);
     //   const signer = provider.getSigner();
@@ -58,61 +58,70 @@
   </button>
 
   <!-- {times.map((time) => ( -->
-    <div class="reddit-card">
-      <div class="post-details">
-        <!-- <small class="subreddit-name"></small> -->
-        <small class="username">posted by u/long_username</small>
-      </div>
+  <div class="reddit-card">
+    <div class="post-details">
+      <!-- <small class="subreddit-name"></small> -->
+      <small class="username">posted by u/long_username</small>
+    </div>
 
-      <div class="reddit-inner-card">
-        <div class="card-votes">
-          <button >
-            <Icon src={BiUpvote} size='23' />
-          </button>
-          <p>9</p>
-          <button >
-            <Icon src={BiDownvote} size='23' />
-          </button>
-        </div>
-        <div class="card-text">
-          <p class="card-title">Genetic doctor recommendation for person with many potential mutations</p>
-          <p class="card-content">Hello, I'm suspected for multiple different genetic disorders, I have dozens of different symptoms and many, many potential mutations.</p>
-        </div>
+    <div class="reddit-inner-card">
+      <div class="card-votes">
+        <button>
+          <Icon src={BiUpvote} size="23" />
+        </button>
+        <p>9</p>
+        <button>
+          <Icon src={BiDownvote} size="23" />
+        </button>
+      </div>
+      <div class="card-text">
+        <p class="card-title">
+          Genetic doctor recommendation for person with many potential mutations
+        </p>
+        <p class="card-content">
+          Hello, I'm suspected for multiple different genetic disorders, I have
+          dozens of different symptoms and many, many potential mutations.
+        </p>
       </div>
     </div>
-    <div class="reddit-card">
-      <div class="post-details">
-        <!-- <small class="subreddit-name"></small> -->
-        <small class="username">posted by u/long_username</small>
-      </div>
+  </div>
+  <div class="reddit-card">
+    <div class="post-details">
+      <!-- <small class="subreddit-name"></small> -->
+      <small class="username">posted by u/long_username</small>
+    </div>
 
-      <div class="reddit-inner-card">
-        <div class="card-votes">
-          <button >
-            <Icon src={BiUpvote} size='23' />
-          </button>
-          <p>9</p>
-          <button >
-            <Icon src={BiDownvote} size='23' />
-          </button>
-        </div>
-        <div class="card-text">
-          <p class="card-title">Genetic doctor recommendation for person with many potential mutations</p>
-          <p class="card-content">Hello, I'm suspected for multiple different genetic disorders, I have dozens of different symptoms and many, many potential mutations.</p>
-        </div>
+    <div class="reddit-inner-card">
+      <div class="card-votes">
+        <button>
+          <Icon src={BiUpvote} size="23" />
+        </button>
+        <p>9</p>
+        <button>
+          <Icon src={BiDownvote} size="23" />
+        </button>
+      </div>
+      <div class="card-text">
+        <p class="card-title">
+          Genetic doctor recommendation for person with many potential mutations
+        </p>
+        <p class="card-content">
+          Hello, I'm suspected for multiple different genetic disorders, I have
+          dozens of different symptoms and many, many potential mutations.
+        </p>
       </div>
     </div>
+  </div>
 </div>
-
 
 <!-- <button on:click={connectWallet}>connect wallet</button> -->
 
 <style>
-  *{
-  padding: 0;
-  margin: 0;
-}
- .reddit-card {
+  * {
+    padding: 0;
+    margin: 0;
+  }
+  .reddit-card {
     max-width: 600px;
     text-align: left;
     /* border: 1px solid black; */
@@ -120,104 +129,102 @@
     padding: 1rem 1rem;
     background: #fefeff;
     border-radius: 0.7rem;
-}
-.btn-createpost {
-  max-width: 200px;
-  width: 100%;
-  margin: 0.5rem;
-  padding: 0.7rem 0.5rem;
-  border: none;
-  background: #1485d6;
-  color: white;
-  border-radius: 0.4rem;
-  opacity: 0.8;
-}
+  }
+  .btn-createpost {
+    max-width: 200px;
+    width: 100%;
+    margin: 0.5rem;
+    padding: 0.7rem 0.5rem;
+    border: none;
+    background: #1485d6;
+    color: white;
+    border-radius: 0.4rem;
+    opacity: 0.8;
+  }
 
-.btn-createpost:hover {
-  opacity: 1;
-}
+  .btn-createpost:hover {
+    opacity: 1;
+  }
 
-.btn-createpost a {
-  font-size: 1rem;
-  color: #fefeff;
-}
+  .btn-createpost a {
+    font-size: 1rem;
+    color: #fefeff;
+  }
   .show-post {
-  margin: 1rem 0;
-  font-family: "Quicksand", sans-serif;
-  text-align: center;
-  background: #dbe0e7;
-  height: 100%;
-  padding: 0.5rem;
-}
+    margin: 1rem 0;
+    font-family: "Quicksand", sans-serif;
+    text-align: center;
+    background: #dbe0e7;
+    height: 100%;
+    padding: 0.5rem;
+  }
 
-.post-details {
-  margin-bottom: 0.3rem;
-}
+  .post-details {
+    margin-bottom: 0.3rem;
+  }
 
-.post-details small {
-  padding: 0.5rem;
-}
+  .post-details small {
+    padding: 0.5rem;
+  }
 
-.post-details .subreddit-name {
-  font-size: 0.9rem;
-  font-weight: bolder;
-  padding-left: 2.5rem;
-}
+  .post-details .subreddit-name {
+    font-size: 0.9rem;
+    font-weight: bolder;
+    padding-left: 2.5rem;
+  }
 
-.post-details .username {
-  color: grey;
-  margin-left: 2rem;
-}
+  .post-details .username {
+    color: grey;
+    margin-left: 2rem;
+  }
 
-/* css for post and upvotes */
+  /* css for post and upvotes */
 
-.reddit-inner-card {
-  display: flex;
-  justify-content: space-between;
-  align-items: flex-start !important;
-}
+  .reddit-inner-card {
+    display: flex;
+    justify-content: space-between;
+    align-items: flex-start !important;
+  }
 
-.card-text {
-  width: 100%;
-  margin: auto 0.5rem;
-}
+  .card-text {
+    width: 100%;
+    margin: auto 0.5rem;
+  }
 
-.card-text .card-title {
-  font-weight: bolder;
-  font-size: 20px;
-  padding-bottom: 0.5rem;
-  border-bottom: 0.2px solid #637680;
-}
+  .card-text .card-title {
+    font-weight: bolder;
+    font-size: 20px;
+    padding-bottom: 0.5rem;
+    border-bottom: 0.2px solid #637680;
+  }
 
-.card-votes {
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  margin: 0px;
-  /* background: #e1e1eb; */
-  border-radius: 1.2rem;
-}
+  .card-votes {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    margin: 0px;
+    /* background: #e1e1eb; */
+    border-radius: 1.2rem;
+  }
 
-.card-votes button {
-  background: transparent;
-  border: none;
-  cursor: pointer;
-  padding: 0.2rem;
-}
+  .card-votes button {
+    background: transparent;
+    border: none;
+    cursor: pointer;
+    padding: 0.2rem;
+  }
 
-.card-content {
-  margin: 0.2rem;
-}
-body{
-  background-color: #637680;
-}
+  .card-content {
+    margin: 0.2rem;
+  }
+  body {
+    background-color: #637680;
+  }
 
-@media only screen and (max-width: 400px) {
-  /* body {
+  @media only screen and (max-width: 400px) {
+    /* body {
     background-color: lightblue;
   } */
-
-
-}
+  }
 </style>
